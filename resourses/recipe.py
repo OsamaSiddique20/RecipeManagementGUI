b598dcbf6db04b3eb7f5a314ac83c22e03703668
Osama <60098546@udst.edu.qa>
2023-11-28 09:47:52 +0300

    Added RecipeListResourse class in Recipe.py of resourses and added get and post method

---
+++ resourses/recipe.py
@@ -0,0 +1,30 @@
+import sys
+from flask import request
+from flask_restful import Resource
+from http import HTTPStatus
+
+from models.recipe import Recipe
+
+
+class RecipeListResource(Resource):
+
+    def get(self):
+        data = Recipe.get_all()
+
+        if data is None:
+            return {'message': 'recipe not found'}, HTTPStatus.NOT_FOUND
+
+        return {'data': data}, HTTPStatus.OK
+
+    def post(self):
+        data = request.get_json()
+
+        recipe = Recipe(name=data['name'],
+                        description=data['description'],
+                        num_of_servings=data['num_of_servings'],
+                        cook_time=data['cook_time'],
+                        directions=data['directions'],
+                        user_id=data["user_id"])
+        recipe.save()
+
+        return recipe.data, HTTPStatus.CREATED
